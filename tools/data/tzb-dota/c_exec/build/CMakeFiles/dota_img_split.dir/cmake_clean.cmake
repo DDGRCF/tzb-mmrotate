ceddc87058a6669c9d1dbf531b3b6187e2e5e67c
DDGRCF <969609856@qq.com>
2023-03-06 09:32:44 +0800

    feat: add custom split

---
+++ tools/data/tzb-dota/c_exec/build/CMakeFiles/dota_img_split.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dota_img_split.dir/src/main.cc.o"
+  "dota_img_split"
+  "dota_img_split.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dota_img_split.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
