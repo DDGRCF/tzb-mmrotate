dde9738a1f787b317c93659913ba9cf7ee38c146
54wb <1225768798@qq.com>
2023-03-15 20:38:00 +0800

    add roi_trans_tzb config

---
+++ configs/roi_trans/roi-trans-le90_r50_fpn_1x_tzb-ship.py
@@ -0,0 +1,87 @@
+_base_ = ['./roi-trans-le90_r50_fpn_1x_dota.py']
+
+angle_version = 'le90'
+model = dict(
+    data_preprocessor=dict(
+        bgr_to_rgb=False),
+    roi_head=dict(
+        bbox_head=[
+            dict(
+                type='mmdet.Shared2FCBBoxHead',
+                predict_box_type='rbox',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=1,
+                reg_predictor_cfg=dict(type='mmdet.Linear'),
+                cls_predictor_cfg=dict(type='mmdet.Linear'),
+                bbox_coder=dict(
+                    type='DeltaXYWHTHBBoxCoder',
+                    angle_version=angle_version,
+                    norm_factor=2,
+                    edge_swap=True,
+                    target_means=(.0, .0, .0, .0, .0),
+                    target_stds=(0.1, 0.1, 0.2, 0.2, 0.1),
+                    use_box_type=True),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='mmdet.CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(
+                    type='mmdet.SmoothL1Loss', beta=1.0, loss_weight=1.0)),
+            dict(
+                type='mmdet.Shared2FCBBoxHead',
+                predict_box_type='rbox',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=1,
+                reg_predictor_cfg=dict(type='mmdet.Linear'),
+                cls_predictor_cfg=dict(type='mmdet.Linear'),
+                bbox_coder=dict(
+                    type='DeltaXYWHTRBBoxCoder',
+                    angle_version=angle_version,
+                    norm_factor=None,
+                    edge_swap=True,
+                    proj_xy=True,
+                    target_means=[0., 0., 0., 0., 0.],
+                    target_stds=[0.05, 0.05, 0.1, 0.1, 0.05]),
+                reg_class_agnostic=False,
+                loss_cls=dict(
+                    type='mmdet.CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(
+                    type='mmdet.SmoothL1Loss', beta=1.0, loss_weight=1.0))
+        ])
+
+)
+
+dataset_type = 'TzbShipDataset'
+data_root = 'data/Tianzhi/ship/'
+train_dataloader = dict(
+    batch_size=8,
+    dataset=dict(
+        type=dataset_type,
+        ann_file='train_split/annfiles/',
+        data_prefix=dict(img_path='train_split/images/'),
+        filter_cfg=dict(filter_empty_gt=False),
+        data_root=data_root))
+
+val_dataloader = dict(
+    dataset=dict(
+        type=dataset_type,
+        ann_file='val_split/annfiles/',
+        data_prefix=dict(img_path='val_split/images/'),
+        data_root=data_root))
+
+val_evaluator = dict(metric='f1_score', iou_thrs=0.1)
+
+train_cfg = dict(val_evaulator=2)
+
+optim_wrapper = dict(
+    type='AmpOptimWrapper',
+    optimizer=dict(lr=0.01)
+)
+
