8d4183f601a96fbcf5136995f848b940a559f09c
DDGRCF <969609856@qq.com>
2023-03-26 21:22:37 +0800

    fix: finish upload 2

--- configs/rotated_rtmdet/rotated_rtmdet_l_tzb-ship.py
+++ configs/rotated_rtmdet/rotated_rtmdet_l_tzb-ship.py
@@ -1,6 +1,6 @@
 _base_ = "./rotated_rtmdet_l-coco_pretrain-3x-dota_ms.py"
 
-file_client_args = dict(backend='disk') # TODO:
+file_client_args = dict(backend='disk')  # TODO:
 img_size = (1024, 1024)
 pad_val = (0, 0, 0)
 
@@ -13,14 +13,8 @@ train_pipeline = [
         type='mmdet.RandomFlip',
         prob=0.75,
         direction=['horizontal', 'vertical', 'diagonal']),
-    dict(
-        type='RandomRotate',
-        prob=0.5,
-        angle_range=180,
-        rect_obj_labels=None),
-    dict(
-        type='mmdet.Pad', size=img_size,
-        pad_val=dict(img=pad_val)),
+    dict(type='RandomRotate', prob=0.5, angle_range=180, rect_obj_labels=None),
+    dict(type='mmdet.Pad', size=img_size, pad_val=dict(img=pad_val)),
     dict(type='mmdet.PackDetInputs')
 ]
 
@@ -30,9 +24,7 @@ val_pipeline = [
     # avoid bboxes being resized
     dict(type='mmdet.LoadAnnotations', with_bbox=True, box_type='qbox'),
     dict(type='ConvertBoxType', box_type_mapping=dict(gt_bboxes='rbox')),
-    dict(
-        type='mmdet.Pad', size=img_size,
-        pad_val=dict(img=pad_val)),
+    dict(type='mmdet.Pad', size=img_size, pad_val=dict(img=pad_val)),
     dict(
         type='mmdet.PackDetInputs',
         meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
@@ -42,9 +34,7 @@ val_pipeline = [
 test_pipeline = [
     dict(type='mmdet.LoadImageFromFile', file_client_args=file_client_args),
     dict(type='mmdet.Resize', scale=img_size, keep_ratio=True),
-    dict(
-        type='mmdet.Pad', size=img_size,
-        pad_val=dict(img=pad_val)),
+    dict(type='mmdet.Pad', size=img_size, pad_val=dict(img=pad_val)),
     dict(
         type='mmdet.PackDetInputs',
         meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
@@ -53,25 +43,25 @@ test_pipeline = [
 model = dict(
     bbox_head=dict(num_classes=1),
     test_cfg=dict(
-        nms_pre=1000, # TODO: ori 2000
+        nms_pre=1000,  # TODO: ori 2000
         min_bbox_size=0,
-        score_thr=0.05, # TODO:
+        score_thr=0.05,  # TODO:
         nms=dict(type='nms_rotated', iou_threshold=0.1),
-        max_per_img=1000)) # 2000
+        max_per_img=1000))  # 2000
 
 dataset_type = "TzbShipDataset"
 data_root = 'data/Tianzhi/ship/'
 img_suffix = ".png"
 
 train_dataloader = dict(
-    batch_size=4, 
+    batch_size=4,
     num_workers=4,
     dataset=dict(
         type=dataset_type,
         img_suffix=img_suffix,
         ann_file='train_split/annfiles/',
         data_prefix=dict(img_path='train_split/images/'),
-        filter_cfg=dict(filter_empty_gt=False), # TODO: True
+        filter_cfg=dict(filter_empty_gt=False),  # TODO: True
         data_root=data_root,
         pipeline=train_pipeline))
 
@@ -92,6 +82,17 @@ checkpoint_config = dict(interval=1)
 _base_.default_hooks["checkpoint"]["interval"] = checkpoint_config["interval"]
 train_cfg = dict(val_interval=36)
 
-optim_wrapper = dict(type='AmpOptimWrapper', optimizer=dict(lr=0.001 / 8)) # TODO:
-val_evaluator = dict(metric='f1_score', iou_thrs=0.1, merge_patches=True, format_only=True, outfile_prefix="test_results")
-test_evaluator = dict(metric='f1_score', iou_thrs=0.1, merge_patches=True, format_only=True, outfile_prefix="test_results")
+optim_wrapper = dict(
+    type='AmpOptimWrapper', optimizer=dict(lr=0.001 / 8))  # TODO:
+val_evaluator = dict(
+    metric='f1_score',
+    iou_thrs=0.1,
+    merge_patches=True,
+    format_only=True,
+    outfile_prefix="work_dirs/test_results")
+test_evaluator = dict(
+    metric='f1_score',
+    iou_thrs=0.1,
+    merge_patches=True,
+    format_only=True,
+    outfile_prefix="work_dirs/test_results")
